b30c77e2e49f0813147db9ddd4edade8dea1c5f1
Mayank Raj <89139319+mayankraj161s@users.noreply.github.com>
2022-04-10 17:54:08 +0530

    Add files via upload

---
+++ stu read.py
@@ -0,0 +1,14 @@
+import pickle
+bfile = open(r"E:\CS\File Handling\Binary Files\data.dat","rb+")
+lst = []
+try:
+    print("File data.dat stores these records:---")
+    while True:
+        lst.append(pickle.load(bfile))
+except EOFError:
+    bfile.close()
+print(lst)
+for i in range(len(lst)):
+    print("Record Number",(i+1),":")
+    for j in range(len(lst[0])):
+        print('\t'+list(lst[i].keys())[j]+" : ",lst[i][list(lst[i].keys())[j]])
---
+++ student.py
@@ -0,0 +1,20 @@
+# Write a program to get student data(roll no., name and marks) from user and write onto a binary file as long as user wants
+import pickle
+bfile = open(r"E:\CS\File Handling\Binary Files\data.dat","wb+")
+stu = {}
+ans = 'y'
+count = 0
+while ans == 'y':
+    rno = int(input("Enter roll number: "))
+    name = input("Enter name: ")
+    marks = float(input("Enter marks: "))
+    # adding data to dict stu{}
+    stu['Rollno.'] = rno
+    stu['Name'] = name
+    stu['Marks'] = marks
+    # now writing into a file
+    pickle.dump(stu, bfile)
+    ans = input("Want to enter more records? (y/n)... ")
+    count += 1
+print(count,"student added.")
+bfile.close()
